97957ebe9a7381be737b94a90673062db4c4ca96
xyngc0de <faiz.adie30@gmail.com>
2022-10-12 21:39:56 +0700

    learn class and constructor, add readme md

---
+++ 01constructor.ts
@@ -0,0 +1,8 @@
+class Animals2 {
+  constructor() {
+    console.log('constructor run first forever');
+  }
+}
+
+const elephant = new Animals2();
+console.log(elephant);
---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Technology
+
+1. typescript
+2. deno run time engine
+3. oop concept
